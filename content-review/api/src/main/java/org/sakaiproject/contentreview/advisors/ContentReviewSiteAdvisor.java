c8aec719286804a8c91afbff14ac0fa122779715
Brian Baillargeon <bbailla2@uwo.ca>
2017-09-14 15:43:34 -0400

    Fixed imports, dependencies, added TiiActivityConfig pojo (replaces ContentReviewActivityConfigEntry) (#39)

--- content-review/api/src/main/java/org/sakaiproject/contentreview/advisors/ContentReviewSiteAdvisor.java
+++ content-review/api/src/main/java/org/sakaiproject/contentreview/advisors/ContentReviewSiteAdvisor.java
@@ -18,5 +18,22 @@ package org.sakaiproject.contentreview.advisors;
 import org.sakaiproject.site.api.Site;
 
 public interface ContentReviewSiteAdvisor {
+
 	public boolean siteCanUseReviewService(Site site);
+
+	public boolean siteCanUseLTIReviewService(Site site);
+
+	// TIITODO: can this be removed now?
+	/**
+	 * Returns true if the TII LTI review service should be used, given the
+	 * assignment creation date. This is a transitional method that should be removed
+	 * once TII legacy api support ends.
+	 * @param site
+	 * @param assignmentCreationDate
+	 * @return
+	 */
+//	@Deprecated
+//	public boolean siteCanUseLTIReviewServiceForAssignment(Site site, Date assignmentCreationDate);
+
+	public boolean siteCanUseLTIDirectSubmission(Site site);
 }
