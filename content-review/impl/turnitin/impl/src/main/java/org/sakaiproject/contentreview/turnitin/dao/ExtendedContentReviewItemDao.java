196e9f72460c7d6e28cb06bb0c03540f16e5a650
plukasew <plukasew@uwo.ca>
2017-09-20 09:33:34 -0400

    more-work, still doesn't build, getting closer (#41)

---
+++ content-review/impl/turnitin/impl/src/main/java/org/sakaiproject/contentreview/turnitin/dao/ExtendedContentReviewItemDao.java
@@ -0,0 +1,32 @@
+package org.sakaiproject.contentreview.turnitin.dao;
+
+import java.util.Calendar;
+import java.util.Optional;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.sakaiproject.contentreview.dao.ContentReviewConstants;
+import org.sakaiproject.contentreview.dao.ContentReviewItem;
+import org.sakaiproject.contentreview.dao.ContentReviewItemDao;
+
+/**
+ *
+ * @author plukasew
+ */
+public class ExtendedContentReviewItemDao<T extends ContentReviewItem> extends ContentReviewItemDao<ContentReviewItem>
+{
+	public Optional<T> findSingleItemToSubmitMissingExternalId(Integer providerId)
+	{
+		Calendar calendar = Calendar.getInstance();
+		
+		Criteria c = sessionFactory.getCurrentSession()
+				.createCriteria(ContentReviewItem.class)
+				.add(Restrictions.eq(PROVIDER_ID_COL, providerId))
+				.add(Restrictions.in(STATUS_COL, new Long[]{ContentReviewConstants.CONTENT_REVIEW_SUBMITTED_AWAITING_REPORT_CODE,
+					ContentReviewConstants.CONTENT_REVIEW_REPORT_ERROR_RETRY_CODE}))
+				.add(Restrictions.isNull(EXTERNAL_ID_COL))
+				.add(Restrictions.lt(NEXT_RETRY_TIME_COL, calendar.getTime()))
+				.setMaxResults(1);
+		
+		return Optional.ofNullable((T) c.uniqueResult());
+	}
+}
