febb791fc7150a31722ca0704953df778bcb6dc5
Brian Baillargeon <bbailla2@uwo.ca>
2017-10-24 10:14:14 -0400

    Connected TiiContentReviewRosterSyncDao in hibernate; not yet injected anywhere (#52)

--- content-review/impl/turnitin/impl/src/main/java/org/sakaiproject/contentreview/turnitin/dao/TiiContentReviewRosterSyncDao.java
+++ content-review/impl/turnitin/impl/src/main/java/org/sakaiproject/contentreview/turnitin/dao/TiiContentReviewRosterSyncDao.java
@@ -1,6 +1,8 @@
 package org.sakaiproject.contentreview.turnitin.dao;
 
 import java.util.Optional;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 import org.sakaiproject.contentreview.dao.HibernateCommonDao;
 import org.sakaiproject.contentreview.model.ContentReviewRosterSyncItem;
 
@@ -10,10 +12,19 @@ import org.sakaiproject.contentreview.model.ContentReviewRosterSyncItem;
  */
 public class TiiContentReviewRosterSyncDao extends HibernateCommonDao<ContentReviewRosterSyncItem>
 {
+	protected static final String SITE_ID_COL = "siteId";
+	protected static final String DATE_QUEUED_COL = "dateQueued";
+	protected static final String LAST_TRIED_COL = "lastTried";
+	protected static final String STATUS_COL = "status";
+	protected static final String MESSAGES_COL = "messages";
+
 	public Optional<ContentReviewRosterSyncItem> findByStatusAndSite(int status, String siteId)
 	{
 		// TIITODO: implement this (BLOCKED BY DECISION TO SUPPORT LEGACY API)
+		Criteria c = sessionFactory.getCurrentSession().createCriteria(ContentReviewRosterSyncItem.class)
+			.add(Restrictions.eq(STATUS_COL, status))
+			.add(Restrictions.eq(SITE_ID_COL, siteId));
 		
-		return Optional.empty();
+		return Optional.ofNullable((ContentReviewRosterSyncItem) c.uniqueResult());
 	}
 }
